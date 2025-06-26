eaa6ddab73c8a7003897cb73331aeea3ca2241bf
ItsWaine14 <jwainebravo@gmail.com>
2025-06-27 00:18:57 +0800

    Main Page major improvement/Added about page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,11 +8,11 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
-<body class="bg-[#dcd6f7] text-[#1b1b18] min-h-screen">
+<body class="bg-gradient-to-b from-[#dcd6f7] to-[#a6b1e1] text-[#1b1b18] min-h-screen">
 
     <header class="w-full">
         <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-            <div class="flex h-16 items-center justify-between">
+            <div class="flex h-16 px-9 items-center justify-between">
 
                 <div class="flex h-16 items-center justify-between">
                     <a href="/" class="flex items-center gap-2">
@@ -21,7 +21,7 @@
                     </a>
                 </div>
 
-                <div class="bg-indigo-900 px-15 py-3 block rounded-lg">
+                <div class="bg-[#424874] px-12 py-3 block rounded-lg ml-10">
                     <nav aria-label="Global">
                         <ul class="flex items-center gap-15 text-sm">
                             <li><a class="text-[#dcd6f7] hover:text-[#ffca22]" href="#">About</a></li>
@@ -75,8 +75,36 @@
     </header>
 
     <main class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 mt-10">
-        <h1 class="text-2xl font-bold">Welcome to Bonzi Bank</h1>
-        <p class="mt-2 text-sm text-gray-600">Secure your future. Start today.</p>
+        <h1 class="text-2xl font-bold text-center ">Welcome to Bonzi Bank</h1>
+        <p class="mt-2 text-sm text-gray-600 text-center ">Secure your future. Start today.</p>
+
+        <div class="m-8 flex flex-wrap gap-10">
+            <div class="min-w-[300px] flex-1 rounded-lg bg-[#424874] p-6 shadow-md">
+                <div class="flex h-full flex-col justify-between text-left text-lg font-semibold text-[#dcd6f7]">
+                <div>
+                    <p class="text-4xl">CREATE AN ACCOUNT</p>
+                    <br />
+                    <p class="text-sm">Get started with a new account and get access to secure banking features such as balance tracking, money transfers, and bill payments.</p>
+                </div>
+                    <a href="{{ route('register') }}">
+                        <button class="mt-6 w-fit self-start rounded-md bg-[#ffca22] px-10 py-2 text-sm font-medium text-[#424874] shadow hover:bg-[#8e9ed6]">
+                            Register
+                        </button>
+                    </a>
+                </div>
+            </div>
+
+            <div class="min-w-[300px] flex-1 rounded-lg bg-[#424874] p-6 shadow-md">
+                <div class="flex h-full flex-col justify-between text-right text-lg font-semibold text-[#dcd6f7]">
+                <div>
+                    <p class="text-4xl">AVAIL A CARD NOW!</p>
+                    <br />
+                    <p class="text-sm">Choose your card type, confirm the details, and start using it for secure transactions.</p>
+                </div>
+                <button class="mt-6 w-fit self-end rounded-md bg-[#ffca22] px-10 py-2 text-sm font-medium text-[#424874] shadow hover:bg-[#8e9ed6]">Get Bonzi Bank Card</button>
+                </div>
+            </div>
+        </div>
     </main>
 
     @livewireScripts
