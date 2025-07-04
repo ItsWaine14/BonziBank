8bcee1d4012e0c74c2142a285372e5a477443a00
Mwahahahahahahaha <matthewnuyda@gmail.com>
2025-07-04 20:35:47 +0800

    properly formatted transaction

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -5,9 +5,9 @@
         Transactions
     </h1>
     {{-- For convenience w/o having to go to check balance (remove if not planning to) --}}
-    <h3 class="text-xl font-bold mb-6 text-center">
+    <h2 class="text-xl font-bold mb-6 text-center">
         Balance: ₱{{ number_format($balance, 2) }}
-    </h3>
+    </h2>
 
     @if(session('message'))
         <div class="mb-4 p-3 text-green-600 rounded-lg">
@@ -15,34 +15,34 @@
         </div>
     @endif
 
-    {{-- Withdraw Form --}}
+    {{-- Withdraw --}}
     <form method="POST" action="{{route('withdraw')}}" class="mb-6">
         @csrf
         <div class="mb-4">
-            <label for="withdraw_amount" class="block text-sm font-medium text-white mb-1">
+            <label for="withdraw_amount" class="block text-xl font-semibold mb-3">
                 Withdraw Amount
             </label>
 
             <input type="number" name="amount" id="withdraw_amount" required class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2">
         </div>
 
-        <button type="submit" class="w-full bg-white text-black font-semibold py-2 px-4 rounded-xl cursor-pointer">
+        <button type="submit" class="w-full bg-[#ffca22] hover:bg-[#dcd6f7] text-[#424874] font-semibold py-2 px-4 rounded-xl cursor-pointer">
             Withdraw
         </button>
     </form>
 
-    {{-- Deposit Form --}}
+    {{-- Deposit --}}
     <form method="POST" action="{{route('deposit')}}">
         @csrf
         <div class="mb-4">
-            <label for="deposit_amount" class="block text-sm font-medium text-white mb-1">
+            <label for="deposit_amount" class="block text-xl font-semibold mb-3">
                 Deposit Amount
             </label>
 
             <input type="number" name="amount" id="deposit_amount" required class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2">
         </div>
 
-        <button type="submit" class="w-full bg-white text-black font-semibold py-2 px-4 rounded-xl cursor-pointer">
+        <button type="submit" class="w-full bg-[#ffca22] hover:bg-[#dcd6f7] text-[#424874] font-semibold py-2 px-4 rounded-xl cursor-pointer">
              Deposit
         </button>
     </form>
@@ -50,16 +50,19 @@
     {{-- History --}}
     @if($transactions->isNotEmpty())
         <div class="mt-10">
-            <h3 class="text-xl font-semibold mb-4">Transaction History</h3>
-            <ul class="max-h-70 overflow-y-auto space-y-2">
+            <h1 class="text-xl font-semibold mb-4">
+                Transaction History
+            </h1>
+
+            <h2 class="max-h-70 overflow-y-auto space-y-2">
                 @foreach($transactions as $i)
-                    <li class="p-3 text-black bg-gray-100 rounded-xl flex justify-between">
+                    <li class="grid grid-cols-3 gap-4 p-3 text-black bg-gray-100 rounded-xl justify-between">
                         <span class="capitalize">{{$i->type}}</span>
-                        <span>₱{{number_format($i->amount, 2)}}</span>
-                        <span class="text-sm text-gray-500">{{$i->created_at->diffForHumans()}}</span>
+                        <span class="text-left">₱{{number_format($i->amount, 2)}}</span>
+                        <span class="text-sm text-gray-500 text-right">{{$i->created_at->diffForHumans()}}</span>
                     </li>
                 @endforeach
-            </ul>
+            </h2>
         </div>
     @endif
 </div>
